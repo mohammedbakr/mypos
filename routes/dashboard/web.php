2ca3880172be8addb157821c9178b1df79c09769
mohammedbakr <bakr.engineer@gmail.com>
2020-02-20 18:37:16 +0200

    initial commit

---
+++ routes/dashboard/web.php
@@ -0,0 +1,18 @@
+<?php
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ],
+    function(){ 
+
+        Route::prefix('dashboard')->name('dashboard.')->group(function(){
+
+            Route::get('/index', 'DashboardController@index')->name('index');
+        
+        
+        }); // end of dashboard routes
+
+    });
+
